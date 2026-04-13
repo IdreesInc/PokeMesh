1b7d595a5a4e1f925ff42ba74d06878c902264bf
Idrees Hassan <idrees@idreesinc.com>
2026-04-13 14:36:53 -0700

    Connect to llm

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
+import json
+import requests
 from pyboy import PyBoy
 
+SETTINGS = json.load(open("settings.json"))
+SERVER = SETTINGS["server"]
+MODEL = SETTINGS["model"]
+
+
 def main():
-	print("Hello, World!")
-	pyboy = PyBoy("resources/yellow.gb")
-	while pyboy.tick():
-		pass
-	pyboy.stop()
+	print(request("Echo 'Hello, World!'"))
+	# pyboy = PyBoy("resources/yellow.gb")
+	# while pyboy.tick():
+	# 	pass
+	# pyboy.stop()
+
+def request(message: str) -> str:
+	response = requests.post(
+		"http://" + SERVER + "/v1/chat/completions",
+		json={
+			"model": MODEL,
+			"messages": [
+				{
+					"role": "user",
+					"content": message
+				}
+			]
+		}
+	)
+	return response.json()["choices"][0]["message"]["content"]
 
 
 if __name__ == "__main__":
