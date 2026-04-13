310f60e86ba09bfe9de005af85979847963c283f
Idrees Hassan <idrees@idreesinc.com>
2026-04-13 16:06:18 -0700

    Take inputs from the console

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import json
 import requests
 from pyboy import PyBoy
 
+class Input:
+	def __init__(self, button: str, times: int = 1):
+		self.button = button
+		self.times = times
+
 SECRETS: dict = json.load(open("secrets.json"))
 SETTINGS: dict = json.load(open("settings.json"))
 SERVER: str = SECRETS["server"]
@@ -10,22 +15,48 @@ MODEL: str = SECRETS["model"]
 PROMPT: str = SETTINGS["prompt"]
 ROM: str = SECRETS["rom"]
 IMG_PATH: str = "tmp/screenshot.png"
+TICKS_PER_INPUT: int = 360
+
+input_queue = list[Input]()
 
 def main():
 	pyboy = PyBoy(ROM)
-	ticks: int = 0
-	TRIGGER_COUNT: int = 1500
-	while pyboy.tick():
-		ticks += 1
-		if ticks % TRIGGER_COUNT == 0:
+	pyboy.tick(600)
+	while True:
+		if len(input_queue) == 0:
 			screenshot = pyboy.screen.image
 			if screenshot:
 				screenshot.save(IMG_PATH)
 				print(summarize(IMG_PATH))
+				process_input(input("> "))
 			else:
 				print("Screenshot failed!")
+		if len(input_queue) > 0:
+			current = input_queue[0]
+			print("Pressing " + current.button)
+			pyboy.button(current.button)
+			current.times -= 1
+			if current.times <= 0:
+				input_queue.pop(0)
+			pyboy.tick(TICKS_PER_INPUT)
 		pass
 	pyboy.stop()
+
+def process_input(command: str):
+	split = command.lower().split()
+	if len(split) == 0:
+		return
+	button = split[0]
+	times = 1
+	if button not in ["up", "down", "left", "right", "a", "b", "start", "select"]:
+		print("Invalid input: " + button)
+		return
+	elif len(split) > 1:
+		try:
+			times = int(split[1])
+		except ValueError:
+			pass
+	input_queue.append(Input(button, times))
 
 def summarize(path: str) -> str:
 	return request(PROMPT, path)
